259b45572bb8e59e6bfb82608bd9f94f597cbd54
ventas-dch <[Your>
2021-05-06 13:37:08 -0400

    nuevo sitio

---
+++ app.js
@@ -0,0 +1,87 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyC8opUCemrtjtveEfuYfu6YwIZc8SXd2lc",
+    authDomain: "usuarios-72650.firebaseapp.com",
+    projectId: "usuarios-72650"
+});
+
+var db = firebase.firestore();
+
+function agregar() {
+    var titulo = document.getElementById("titulo").value;
+    var tipo = document.getElementById("tipo").value;
+    var descripcion = document.getElementById("descripcion").value;
+    db.collection("datos").add({
+        title: titulo,
+        type: tipo,
+        description: descripcion
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            document.getElementById("titulo").value = '';
+            document.getElementById("tipo").value = '';
+            document.getElementById("descripcion").value = '';
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
+var tabla = document.getElementById("tabla");
+db.collection("datos").onSnapshot((querySnapshot) => {
+    tabla.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        tabla.innerHTML += `
+        <tr>
+            <td class="center">${doc.data().title}</td>
+            <td>${doc.data().type}</td>
+            <td>${doc.data().description}</td>
+            <td><button class="btn red darken-2" style="border-radius: 20px; font-family: 'Sriracha', cursive; background: url(img/black-felt.png);" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+            <td><button class="btn yellow darken-2" style="border-radius: 20px; font-family: 'Sriracha', cursive; background: url(img/black-felt.png);" onclick="editar('${doc.id}','${doc.data().title}','${doc.data().type}','${doc.data().description}')">Editar</button></td>
+        </tr>
+        `
+    });
+});
+function eliminar(id) {
+    db.collection("datos").doc(id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+}
+
+function editar(id, titulo, tipo, descripcion) {
+
+    document.getElementById('titulo').value = titulo;
+    document.getElementById('tipo').value = tipo;
+    document.getElementById('descripcion').value = descripcion;
+    var boton = document.getElementById('boton');
+    boton.innerHTML = 'Editar'
+    boton.onclick = function () {
+        var washingtonRef = db.collection("datos").doc(id);
+        // Set the "capital" field of the city 'DC'
+        var titulo = document.getElementById('titulo').value;
+        var tipo = document.getElementById('tipo').value;
+        var descripcion = document.getElementById('descripcion').value;
+        return washingtonRef.update({
+            title: titulo,
+            type: tipo,
+            description: descripcion
+        })
+            .then(() => {
+                console.log("Document successfully updated!");
+                boton.innerHTML = 'Agregar'
+                document.getElementById('titulo').value = '';
+                document.getElementById('tipo').value = '';
+                document.getElementById('descripcion').value = '';
+               
+               
+            })
+            .catch((error) => {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+            });
+    }
+
+}
+
---
+++ user.js
@@ -0,0 +1,78 @@
+var btn1 = document.getElementById("btn1");
+
+btn1.addEventListener('click', ingreso)
+function ingreso() {
+    var email1 = document.getElementById("email1").value;
+    var clave1 = document.getElementById("clave1").value;
+
+    firebase.auth().signInWithEmailAndPassword(email1, clave1)
+        .then((userCredential) => {
+            var user = userCredential.user;
+            document.getElementById("email1").value = "";
+            document.getElementById("clave1").value = "";
+            M.toast({ html: "Haz iniciado sesion con exito!" })
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            M.toast({html: error.message})
+        });
+}
+function comprobar() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            mostrar(user);
+            console.log("****************");
+            console.log(user.emailVerified);
+            console.log("****************");
+            var uid = user.uid;
+            var emailVerified = user.emailVerified;
+
+        } else {
+            console.log("no existe un usuario activo");
+
+        }
+    });
+
+}
+comprobar();
+function cerrarSesion() {
+    firebase.auth().signOut()
+        .then(function () {
+            location.reload(1000);
+            console.log("Saliendo..."); 
+            M.toast({html:"Haz cerrado sesion con exito!"})
+
+        })
+        .catch(function (error) {
+            console.log(error);
+           
+        })
+
+}
+function mostrar(user) {
+    var user = user;
+    var buttons = document.getElementById("buttons");
+
+    if (user.emailVerified) {
+
+        buttons.innerHTML = `
+        
+        <div class="col l10 offset-l1" >
+            <a href="Add.html"><button class="btn blue darken-2 left" style="background: url(img/black-felt.png); margin-right: 10px; font-family: 'Sriracha', cursive; border-radius: 25px" >Ingresar al sistema</button></a>
+
+            <button class="btn red darken-2 left" style="background: url(img/black-felt.png); font-family: 'Sriracha', cursive;  border-radius: 25px">Usuario: ${user.email}</button>
+                
+            <button class="btn blue darken-2 left" style="background: url(img/black-felt.png);margin-left: 10px; font-family: 'Sriracha', cursive; border-radius: 25px" onclick="cerrarSesion()">Cerrar</button>
+
+            
+           
+
+            
+        </div>
+
+        
+        
+        `;
+    }
+}
